a8fdd7b058ca385104dd64dd8b577cffa88488af
Ferronn <ferronn@ferronn.dev>
2021-02-10 14:22:45 +0000

    remove RegisterSlashCommand

--- core.lua
+++ core.lua
@@ -2,16 +2,6 @@ local _, G = ...
 assert(not G.Core)
 G.Core = {}
 
--- Registering a slash command is funny business---it requires a globally
--- unique variable name and globally unique slash command names. For this
--- addon we just jump in head first and hope for the best.
-function G.Core.RegisterSlashCommand(slashes, var, func)
-  for i, slash in ipairs(slashes) do
-    _G['SLASH_' .. var .. i] = slash
-  end
-  SlashCmdList[var] = func
-end
-
 -- Creats a frame that listens for events named for the members of [events],
 -- and dispatches to those functions.
 function G.Core.RegisterEventHandlers(events)
